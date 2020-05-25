8fb95c34d7d4358b2b3de78bf572fa0062980835
Finrod Chen <s02260441@gmail.com>
2020-05-25 20:44:28 +0000

    Initial commit

---
+++ taiex_fetch.py
@@ -0,0 +1,21 @@
+import twstock
+import pandas as pd
+# 導入twstock及pandas模組，pandas模組縮寫為pd
+
+target_stock = '4142'  #股票代號變數
+stock = twstock.Stock(target_stock)  #告訴twstock我們要查詢的股票
+target_price = stock.fetch_from(2020, 5)  #取用2020/05至今每天的交易資料
+
+name_attribute = [
+    'Date', 'Capacity', 'Turnover', 'Open', 'High', 'Low', 'Close', 'Change',
+    'Trascation'
+]  #幫收集到的資料設定表頭
+
+df = pd.DataFrame(columns=name_attribute, data=target_price)
+#將twstock抓到的清單轉成Data Frame格式的資料表
+
+filename = f'./data/{target_stock}.csv'
+#指定Data Frame轉存csv檔案的檔名與路徑
+
+df.to_csv(filename)
+#將Data Frame轉存為csv檔案
