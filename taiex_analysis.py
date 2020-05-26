8538bd8b7e47987e5ce8366cb50ee96278993d35
Finrod Chen <finrodchen@guangli.com.tw>
2020-05-26 18:47:59 +0800

    major update

--- taiex_analysis.py
+++ taiex_analysis.py
@@ -1,20 +1,23 @@
-import pandas
-import mplfinance as mpf
+import pandas as pd
 import matplotlib
-import matplotlib.pyplot as plt
+import mplfinance as mpf
+# 導入pandas、matplotlib、mplfinance模組，將mplfinance模組縮寫為mpf
+# 這邊要導入matplotlib的原因是因為mplfinance繪圖時需要調用mptplotlib模組
 
-matplotlib.style.use('ggplot')
+target_stock = '0050' #設定要繪製走勢圖的股票
 
-stock_sid = '0050'
+df = pd.read_csv(f'./data/{target_stock}.csv', parse_dates=True, index_col=1) #讀取目標股票csv檔的位置
 
-history = pandas.read_csv(
-    f'./data/{stock_sid}.csv', parse_dates=True, index_col=1)
+df.rename(columns={'Turnover':'Volume'}, inplace = True) 
+#這裡針對資料表做一下修正，因為交易量(Turnover)在mplfinance中須被改為Volume才能被認出來
 
-mpf.plot(
-    history,
-    type="candle",
-    mav=(5, 10),
-    style='charles',
-    title=stock_sid,
-    ylabel="OHLC",
-)
+mc = mpf.make_marketcolors(up='r',down='g',inherit=True)
+s  = mpf.make_mpf_style(base_mpf_style='yahoo',marketcolors=mc)
+#針對線圖的外觀微調，將上漲設定為紅色，下跌設定為綠色，符合台股表示習慣
+#接著把自訂的marketcolors放到自訂的style中，而這個改動是基於預設的yahoo外觀
+
+kwargs = dict(type='candle', mav=(5,20,60), volume=True, figratio=(10,8), figscale=0.75, title=target_stock, style=s) 
+#設定可變參數kwargs，並在變數中填上繪圖時會用到的設定值
+
+mpf.plot(df, **kwargs)
+#選擇df資料表為資料來源，帶入kwargs參數，畫出目標股票的走勢圖
